31b9475f933cd6e8208cd64ed14b776b7570d2ee
Fernando <Fernando@DESKTOP-93A7C55>
2021-01-03 18:46:28 -0300

    Creacion de la clase main

--- src/Test/main.java
+++ src/Test/main.java
@@ -5,10 +5,35 @@
  */
 package Test;
 
+import Compartido.Aeropuerto;
+import Hilos.Pasajero;
+import Hilos.Reloj;
+
 /**
  *
- * @author Fernando
+ * @author Fernando Iraira <fmiraira@gmail.com>
  */
 public class main {
-    
+
+    public static void main(String[] args) {
+        int cantPasajeros = 10;
+        int cantAerolineas = 10;
+        int capPuestosAtencion = 1;
+
+        int capMaxTren = 5;
+
+        Aeropuerto aerolinea = new Aeropuerto(cantAerolineas, capMaxTren, capPuestosAtencion);
+
+        Reloj r = new Reloj(aerolinea);
+        Thread reloj = new Thread(r, "RELOJ");
+        reloj.start();
+
+        for (int i = 1; i <= cantPasajeros; i++) {
+            Pasajero p = new Pasajero(aerolinea);
+            Thread pasajero = new Thread(p, "PASAJERO " + i);
+            pasajero.start();
+        }
+
+    }
+
 }
